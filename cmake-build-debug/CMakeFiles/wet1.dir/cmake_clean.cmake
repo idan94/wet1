b597cc621e410d3c2eed6a0e0003bb3ef56b2bbc
IdanCohen <36813204+idan94@users.noreply.github.com>
2018-11-23 08:10:09 +0200

    Idan
    uploading to site

---
+++ cmake-build-debug/CMakeFiles/wet1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wet1.dir/main.cpp.obj"
+  "wet1.pdb"
+  "wet1.exe"
+  "wet1.exe.manifest"
+  "libwet1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wet1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
